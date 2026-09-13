90eaa83cfaf2dc86da6fe7134f7f40c2e2a1b5f8
S-Astralcoder <suraj.astralcode.ai@gmail.com>
2026-09-13 20:08:19 +0530

    feat: Implement signup functionality and enhance login page design

--- static/js/index.js
+++ static/js/index.js
@@ -3,10 +3,10 @@ function redirect_login() {
     let get_started_btn2 = document.querySelector("#nav-start-btn")
     if (get_started_btn1 instanceof HTMLButtonElement && get_started_btn2 instanceof HTMLButtonElement) {
         get_started_btn1.addEventListener("click", () => {
-            window.location.href = "login.html"
+            window.location.href = "signup.html"
         })
         get_started_btn2.addEventListener("click", () => {
-            window.location.href = "login.html"
+            window.location.href = "signup.html"
         })
     }
 }
