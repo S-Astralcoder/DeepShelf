03800b4763008cb4f0437e5a8a716f46b61e341c
S-Astralcoder <suraj.astralcode.ai@gmail.com>
2026-09-13 17:24:52 +0530

    feat: Add login functionality and enhance navigation structure

---
+++ static/js/index.js
@@ -0,0 +1,14 @@
+function redirect_login() {
+    let get_started_btn1 = document.querySelector("#start-btn")
+    let get_started_btn2 = document.querySelector("#nav-start-btn")
+    if (get_started_btn1 instanceof HTMLButtonElement && get_started_btn2 instanceof HTMLButtonElement) {
+        get_started_btn1.addEventListener("click", () => {
+            window.location.href = "login.html"
+        })
+        get_started_btn2.addEventListener("click", () => {
+            window.location.href = "login.html"
+        })
+    }
+}
+
+redirect_login()
